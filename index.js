f6129f0087ebcdd0c193765731c1284908c93474
d-Akkya <akhileshbamane26@gmail.com>
2025-07-09 23:48:11 +0530

    add comment api and routes

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import userRoutes from "./routes/user.route.js";
 import postRoutes from "./routes/post.route.js";
+import commentRoutes from "./routes/comment.route.js";
 import "dotenv/config";
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -12,6 +13,7 @@ app.use(express.urlencoded({ extended: false }));
 // apis
 app.use("/api/v1/user", userRoutes);
 app.use("/api/v1/post", postRoutes);
+app.use("/api/v1/comment", commentRoutes);
 
 app.get("/", (_, res) => {
   return res.send("<h1>Hello World!</h1>");
